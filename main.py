709f5faaa80b440d6398b22cbbd51ba7036ecefd
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2022-08-04 13:04:05 -0400

    adding comments & removing unnessary code

--- main.py
+++ main.py
@@ -19,29 +19,19 @@ def validate(date_text):
         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
 
 
-# class GlucoseDataPoint:
-#     def __init__(self, timestamp, value):
-#         self.timestamp = timestamp
-#         self.value = value
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-
+# model for glucose data points
 class GlucoseDataPoint(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     timestamp: int
     value: float
-    
+    patientId: str
 
 app = FastAPI()
 
 # async await for api reqs
 client = httpx.AsyncClient(verify=False)
-
-# body object for glucose api call
-class GlucoseBody(BaseModel):
-    bearerToken: str
-    patientId: str
-    professionalId: str
 
 # main function that'll return all the data requested from the data pool
 @app.get("/glucose")
@@ -62,25 +52,165 @@ async def get_glucose(start_date: str, end_date: str):
     # convert it to int to remove trailing 0 & decimal
     epochStart = int(datetime.datetime.strptime(start_date, "%Y-%m-%d").timestamp())
     epochEnd = int(datetime.datetime.strptime(end_date, "%Y-%m-%d").timestamp())
-    print("Start " + str(epochStart))
-    print("End " + str(epochEnd))
-    # validate the bearerToken isnt expired
-    
-    # ensure the data has been loaded
-        # if not, prompt user to call the /generate-data endpoint 
     
     engine = create_engine("sqlite:///GlucoseData.db")
-
+    # get datapoints within bounds
     with Session(engine) as session:
         statement = select(GlucoseDataPoint).where(GlucoseDataPoint.timestamp > epochStart).where(GlucoseDataPoint.timestamp < epochEnd)
         glucose = session.exec(statement).all()
-        # print(glucose)
-    # search data pool based on parameters passed in
-    # data = await query_libre(epochStart, epochEnd, glucoseBody.bearerToken, glucoseBody.patientId, glucoseBody.professionalId)
-
-    # data = "testing"
+    # return those datapoints
     return {"data": glucose}
     
+
+
+class SignInObject(BaseModel):
+    email: str
+    password: str
+    
+class EnterCodeObject(BaseModel):
+    accessCode: int
+    bearerToken: str
+
+# sign in function that will pass take a username & password & send it to 
+# LibreView
+@app.post("/signin")
+async def sign_in_user(creds: SignInObject):
+
+    # create body object
+    signInBody={
+        "Country":"GB",
+        "email": creds.email,
+        "password": creds.password
+        }
+    # convert to json
+    jsonData = json.dumps(signInBody)
+
+    try:
+        headers = {"Content-Type":"application/json", "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
+        callback_url = "https://api-eu.libreview.io/auth/login"
+        initialSignIn = await client.post(callback_url, data=jsonData, headers=headers)
+        loginResponse = initialSignIn.json()
+        bearerToken = loginResponse["data"]["authTicket"]["token"]
+        verify = loginResponse["data"]["step"]
+    except httpx.RequestError as exc:
+        return { "error": f"An error occurred while requesting {exc.request.url!r}." }
+    except httpx.HTTPStatusError as exc:
+        return { "error": f"Error response {exc.response.status_code} while requesting {exc.request.url!r}." }
+    except:
+        return {"error": f"Error response {initialSignIn.content} while requesting {callback_url}." }
+
+    # IF the user is not remembered (needs confirmation token)
+    
+    if verify["type"] == "2faVerify":
+        # create body object
+        verifyObject={
+            "isPrimaryMethod":False
+            }
+        # convert to json
+        jsonData = json.dumps(verifyObject)
+        try:
+            # generate the access token to be sent to the email
+            headers = {'Authorization': 'Bearer ' + bearerToken, "Content-Type":"application/json", "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
+            callback_url = "https://api-eu.libreview.io/auth/continue/2fa/sendcode"
+            generateCode = await client.post(callback_url, data=jsonData, headers=headers)
+            sendCodeResponse = generateCode.json()
+            # this generates a NEW bearerToken as well
+            bearerToken = sendCodeResponse["ticket"]["token"]
+            # print(sendCodeResponse)
+        except httpx.RequestError as exc:
+            return { "error": f"An error occurred while requesting {exc.request.url!r}." }
+        except httpx.HTTPStatusError as exc:
+            return { "error": f"Error response {exc.response.status_code} while requesting {exc.request.url!r}." }
+        except:
+            return {"error": f"Error response {generateCode.content} while requesting {callback_url}." }
+        
+    # return here & tell the user to call the endpoint to send the code to libreview
+
+    return {"data": f"call the /enter-code endpoint & in the body pass the code & bearerToken {bearerToken}"}
+    
+# sign in function that will pass take a username & password & send it to 
+# LibreView
+@app.post("/enter-code")
+async def enter_code(token: EnterCodeObject):
+    
+    # create body object
+    verifyObject={
+        "code": str(token.accessCode),
+        "isPrimaryMethod": False,
+        }
+    # convert to json
+    jsonData = json.dumps(verifyObject)
+    try:
+        headers = {'Authorization': 'Bearer ' + token.bearerToken, "Content-Type":"application/json", "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
+        callback_url = "https://api-eu.libreview.io/auth/continue/2fa/result"
+        finalSignIn = await client.post(callback_url, data=jsonData, headers=headers)
+        loginResponse = finalSignIn.json()
+        bearerToken = loginResponse["data"]["authTicket"]["token"]
+        professionalId = loginResponse["data"]["user"]["id"]
+    except httpx.RequestError as exc:
+        return { "error": f"An error occurred while requesting {exc.request.url!r}." }
+    except httpx.HTTPStatusError as exc:
+        return { "error": f"Error response {exc.response.status_code} while requesting {exc.request.url!r}." }
+    except:
+        return {"error": f"Error response {finalSignIn.content} while requesting {callback_url}." }
+
+    dashboardObject={
+        "filters":[],
+        "columns":[
+            "lastAvailableData",
+            "avgGlucose",
+            "sensorDailyScans",
+            "percentGlucoseInTarget",
+            "libreViewStatus"
+            ],
+        "interval":"14",
+        "view":"Dashboard.allPatients",
+        "count":50,
+        "page":1,
+        "searching":{},
+        "sort":{
+            "desc":False,"key":"firstName"
+        }
+    }
+    # convert to json
+    jsonData = json.dumps(dashboardObject)
+
+    # call /dashboard to get the patientIds & information 
+    try:
+        headers = {'Authorization': 'Bearer ' + bearerToken, "Content-Type":"application/json", "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
+        callback_url = "https://api-eu.libreview.io/dashboard"
+        dashboardData = await client.post(callback_url, data=jsonData, headers=headers)
+        dashboardResponse = dashboardData.json()
+        # get patient info
+        patients = dashboardResponse["data"]["results"]
+        patientIds = []
+        # iterate thru & collect all patient ids
+        for patient in patients:
+            patientIds.append(patient["id"])
+    except httpx.RequestError as exc:
+        return { "error": f"An error occurred while requesting {exc.request.url!r}." }
+    except httpx.HTTPStatusError as exc:
+        return { "error": f"Error response {exc.response.status_code} while requesting {exc.request.url!r}." }
+    except:
+        return {"error": f"Error response {dashboardData.content} while requesting {callback_url}." }
+
+
+    # for each patient, add their information to the db
+    
+    # get last 6 months of data
+    # calc the date today in epoch
+    todayDate = int(time.time())
+    # 1 month = 2629743
+    # 1 month x 6
+    differenceInMonths = int(2629743 * 6)
+    startDate = todayDate - differenceInMonths
+    
+    for patient in patientIds:
+        await query_libre(startDate,todayDate,bearerToken,patient,professionalId)
+
+    return {"data": "You can now query the /glucose endpoint to get the data you need"}
+
+# this function querys LibreView to access the data for each patient
 async def query_libre(start_date: int, end_date: int, bearerToken: str, patientId: str, professionalId: str):
     
     # calc the date today in epoch
@@ -200,170 +330,14 @@ async def query_libre(start_date: int, end_date: int, bearerToken: str, patientI
             for dataGroup in day["Glucose"]:
                 # this is each individial piece of data
                 for dataPoint in dataGroup:
-                    
-                    glucose = GlucoseDataPoint(timestamp=dataPoint["Timestamp"],value=dataPoint["Value"])
+                    # create a model instance 
+                    glucose = GlucoseDataPoint(timestamp=dataPoint["Timestamp"],value=dataPoint["Value"],patientId=patientId)
+                    # save it 
                     with Session(engine) as session:
                         session.add(glucose)
                         session.commit()
-                    # glucoseData.append(glucose)
     except:
         return {"error": "there was an issue parsing the data returned from the api"}
 
-
-
     return {"data":"the data was successfully stored"}
-
-
-class SignInObject(BaseModel):
-    email: str
-    password: str
-    
-class EnterCodeObject(BaseModel):
-    accessCode: int
-    bearerToken: str
-
-# sign in function that will pass take a username & password & send it to 
-# LibreView
-@app.post("/signin")
-async def sign_in_user(creds: SignInObject):
-    
-    # may need to call this func to set to gb
-    # callback_url = "https://api-eu.libreview.io/config"
-        
-    # create body object
-    signInBody={
-        "Country":"GB",
-        "email": creds.email,
-        "password": creds.password
-        }
-    # convert to json
-    jsonData = json.dumps(signInBody)
-
-    try:
-        headers = {"Content-Type":"application/json", "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
-        callback_url = "https://api-eu.libreview.io/auth/login"
-        initialSignIn = await client.post(callback_url, data=jsonData, headers=headers)
-        loginResponse = initialSignIn.json()
-        bearerToken = loginResponse["data"]["authTicket"]["token"]
-        verify = loginResponse["data"]["step"]
-        print(loginResponse)
-    except httpx.RequestError as exc:
-        return { "error": f"An error occurred while requesting {exc.request.url!r}." }
-    except httpx.HTTPStatusError as exc:
-        return { "error": f"Error response {exc.response.status_code} while requesting {exc.request.url!r}." }
-    except:
-        return {"error": f"Error response {initialSignIn.content} while requesting {callback_url}." }
-
-    # IF the user is not remembered (needs confirmation token)
-    
-    if verify["type"] == "2faVerify":
-        # create body object
-        verifyObject={
-            "isPrimaryMethod":False
-            }
-        # convert to json
-        jsonData = json.dumps(verifyObject)
-        try:
-            # generate the access token to be sent to the email
-            headers = {'Authorization': 'Bearer ' + bearerToken, "Content-Type":"application/json", "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
-            callback_url = "https://api-eu.libreview.io/auth/continue/2fa/sendcode"
-            generateCode = await client.post(callback_url, data=jsonData, headers=headers)
-            sendCodeResponse = generateCode.json()
-            # this generates a NEW bearerToken as well
-            bearerToken = sendCodeResponse["ticket"]["token"]
-            # print(sendCodeResponse)
-        except httpx.RequestError as exc:
-            return { "error": f"An error occurred while requesting {exc.request.url!r}." }
-        except httpx.HTTPStatusError as exc:
-            return { "error": f"Error response {exc.response.status_code} while requesting {exc.request.url!r}." }
-        except:
-            return {"error": f"Error response {generateCode.content} while requesting {callback_url}." }
-        
-    # return here & tell the user to call the endpoint to send the code to libreview
-
-    return {"data": f"call the /enter-code endpoint & in the body pass the code & bearerToken {bearerToken}"}
-    
-# sign in function that will pass take a username & password & send it to 
-# LibreView
-@app.post("/enter-code")
-async def enter_code(token: EnterCodeObject):
-    
-    # create body object
-    verifyObject={
-        "code": str(token.accessCode),
-        "isPrimaryMethod": False,
-        }
-    # convert to json
-    jsonData = json.dumps(verifyObject)
-    try:
-        headers = {'Authorization': 'Bearer ' + token.bearerToken, "Content-Type":"application/json", "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
-        callback_url = "https://api-eu.libreview.io/auth/continue/2fa/result"
-        finalSignIn = await client.post(callback_url, data=jsonData, headers=headers)
-        loginResponse = finalSignIn.json()
-        bearerToken = loginResponse["data"]["authTicket"]["token"]
-        professionalId = loginResponse["data"]["user"]["id"]
-    except httpx.RequestError as exc:
-        return { "error": f"An error occurred while requesting {exc.request.url!r}." }
-    except httpx.HTTPStatusError as exc:
-        return { "error": f"Error response {exc.response.status_code} while requesting {exc.request.url!r}." }
-    except:
-        return {"error": f"Error response {finalSignIn.content} while requesting {callback_url}." }
-
-    dashboardObject={
-        "filters":[],
-        "columns":[
-            "lastAvailableData",
-            "avgGlucose",
-            "sensorDailyScans",
-            "percentGlucoseInTarget",
-            "libreViewStatus"
-            ],
-        "interval":"14",
-        "view":"Dashboard.allPatients",
-        "count":50,
-        "page":1,
-        "searching":{},
-        "sort":{
-            "desc":False,"key":"firstName"
-        }
-    }
-    # convert to json
-    jsonData = json.dumps(dashboardObject)
-
-    # call /dashboard to get the patientIds & information 
-    try:
-        headers = {'Authorization': 'Bearer ' + bearerToken, "Content-Type":"application/json", "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
-        callback_url = "https://api-eu.libreview.io/dashboard"
-        dashboardData = await client.post(callback_url, data=jsonData, headers=headers)
-        dashboardResponse = dashboardData.json()
-        # get patient info
-        patients = dashboardResponse["data"]["results"]
-        patientIds = []
-        # iterate thru & collect all patient ids
-        for patient in patients:
-            patientIds.append(patient["id"])
-    except httpx.RequestError as exc:
-        return { "error": f"An error occurred while requesting {exc.request.url!r}." }
-    except httpx.HTTPStatusError as exc:
-        return { "error": f"Error response {exc.response.status_code} while requesting {exc.request.url!r}." }
-    except:
-        return {"error": f"Error response {dashboardData.content} while requesting {callback_url}." }
-
-
-    # for each patient, add their information to the db
-    
-    # get last 6 months of data
-    # calc the date today in epoch
-    todayDate = int(time.time())
-    # 1 month = 2629743
-    # 1 month x 6
-    differenceInMonths = int(2629743 * 6)
-    startDate = todayDate - differenceInMonths
-    
-    for patient in patientIds:
-        await query_libre(startDate,todayDate,bearerToken,patient,professionalId)
-        print("got data for patient " + patient)
-
-    return {"data": "finished"}
-
 
